119cc203dc6af487eb66181259f89288faefeeed
wdalbock <126948288+wdalbock@users.noreply.github.com>
2024-04-27 13:40:18 +0800

    Added very basic page to submit images

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
@@ -12,3 +13,7 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6), EqualTo('confirm_password', message='Passwords must match')])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+
+class ImageForm(FlaskForm):
+    image = FileField('Image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only')])
+    submit = SubmitField('Upload')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
+from app.forms import ImageForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('home.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    form = ImageForm()
+    return render_template('create_post.html', form=form)
