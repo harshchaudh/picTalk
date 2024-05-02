9a904b79a4b32902c02036497bad0f1ac21e3ddb
wdalbock <126948288+wdalbock@users.noreply.github.com>
2024-05-02 10:59:48 +0800

    Made seperate JS file, added tag addition system

---
+++ app/static/javascript/create_post.js
@@ -0,0 +1,57 @@
+//Image preview
+
+$(document).ready(function() {
+    $('#file-upload').change(function(event) {
+        var input = this;
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#img-preview').attr('src', e.target.result).css('display', 'block');
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+});
+
+//Tag addition
+
+let $input = $('#tag-text-input');
+let $tagForm = $('#form');
+let $output = $('.tags');
+
+function outputTag() {
+    const tag = `
+    <span class="tag">
+        <b>${$input.val()}</b>
+        <span class="material-symbols-outlined remove-btn">
+            close
+        </span>
+    </span>
+    `;
+
+    $output.append(tag);
+    $input.val("");
+}
+
+$tagForm.keydown(function(e) { //Event listener on the enter button to see if a tag has been submitted
+    if (e.which == 13) {
+        e.preventDefault();
+        if($output.children().length >= 4) {
+            outputTag();
+            $input.prop('disabled', true);
+            $input.val("");
+            $input.attr('placeholder', "Max number of tags reached!");
+        }
+        else {
+            outputTag();
+        }
+    }
+});
+
+$(window).on('click', function(e) { //Event listener on tag remove button to remove tags and allow for a new one to be made
+    if(e.target.classList.contains('remove-btn')) {
+        e.target.parentElement.remove();
+        $input.prop('disabled', false);
+        $input.attr('placeholder', "Add a tag...");
+    }
+})
