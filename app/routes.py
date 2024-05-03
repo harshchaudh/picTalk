730d5e7fdacb361fe59bc2ba51c1d099ce062a38
harshchaudh <harsh.hc012@gmail.com>
2024-05-03 15:02:59 +0800

    Minor fix with import path.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from app.forms import CreateContentForm
+from forms import CreateContentForm
 from werkzeug.utils import secure_filename
 import os
 
