5cc90f57e51db91f2415fdb5463af50ca615b397
harshchaudh <harsh.hc012@gmail.com>
2024-05-03 16:57:48 +0800

    Continuing to implement submission page.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo
 
