26c2293fb4d9357e2a73ca26ec5750c932e2f000
harshchaudh <harsh.hc012@gmail.com>
2024-05-06 12:11:06 +0800

    Updated model.

--- app/model.py
+++ app/model.py
@@ -13,3 +13,27 @@ class USER(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+# SUBMISSION table in picTalk.db
+class SUBMISSION(db.Model):
+    __tablename__ = 'SUBMISSION'
+
+    submission_id = db.Column(db.String(20), primary_key=True)
+
+    username = db.Column(db.String(20), db.ForeignKey('USER.username'), nullable=False)
+    user = db.relationship('USER', backref='SUBMISSION', lazy=True)
+
+    # Additional columns required image, caption and tags. Perhaps extra fields not sure.
+
+# COMMENT table in picTalk.db
+class COMMENT(db.Model):
+    __tablename__ = 'COMMENT'
+
+    comment_id  = db.Column(db.String(20), primary_key=True)
+    comment = db.Column(db.String(20), nullable=False)
+
+    username = db.Column(db.String(20), db.ForeignKey('USER.username'), nullable=False)
+    user = db.relationship('USER', backref='COMMENT', lazy=True)
+    
+    submission_id = db.Column(db.String(20), db.ForeignKey('SUBMISSION.submission_id'), nullable=False)
+    submit = db.relationship('SUBMISSION', backref='COMMENT', lazy=True)
