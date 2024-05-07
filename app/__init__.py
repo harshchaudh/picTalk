bb76e5dbd0ea83fc134149cbf5652a7a0b4f2c62
harshchaudh <harsh.hc012@gmail.com>
2024-05-07 11:12:02 +0800

    Removed bcrypt and and updated config.py.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(Config)
     
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
 with app.app_context():
     db.create_all()
--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
 
 class Config:
+    SECRET_KEY = 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///picTalk.db'
