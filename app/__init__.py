b6821024f7d3c062df92406dc1c65a87f7c0e378
harshchaudh <harsh.hc012@gmail.com>
2024-05-07 11:26:09 +0800

    Fixed issues with merge.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
 from flask import Flask
-from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from model import db, init_login
+from routes import picTalk_bp
+
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
     
-db = SQLAlchemy(app)
+# Initialize SQLAlchemy
+db.init_app(app)
+
+# Initialize Flask-Login
+login_manager = LoginManager(app)
+login_manager.login_view = 'picTalk.login'
+
+# Initialize Flask-Login in the model module
+init_login(login_manager)
+
+app.register_blueprint(picTalk_bp)
 
 with app.app_context():
     db.create_all()
-
-from app.model import USER
-from app.routes import picTalk_bp
-app.register_blueprint(picTalk_bp)
