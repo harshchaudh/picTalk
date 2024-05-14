1233fb6cb62edd68fbcc1cd9b6720cc9a955c576
wdalbock <126948288+wdalbock@users.noreply.github.com>
2024-05-14 11:15:11 +0800

    Added time differences to comments

--- app/utilities.py
+++ app/utilities.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import re
 from wtforms import ValidationError, validators
 from app.model import USER 
@@ -70,3 +71,24 @@ def format_profileNumbers(value):
     
 def organiseColumnImages(elements):
     return elements[::3], elements[1::3], elements[2::3]
+
+def truncate_comment_time(comment_time):
+    time_diff = (datetime.now() - comment_time)
+    day_diff = time_diff.days
+    
+    yrs = day_diff // 365
+    months = (day_diff % 365) // 30
+
+    if yrs > 0:
+        return f"{yrs} years ago"
+    elif months > 0:
+        return f"{months} months ago"
+    elif day_diff > 1:
+        return f"{day_diff} days ago"
+    else:
+        hrs = time_diff.seconds // 3600
+        mins = (time_diff.seconds % 3600) // 60
+        if hrs > 0:
+            return f"{hrs} hours ago"
+        else:
+            return f"{mins} minutes ago"
