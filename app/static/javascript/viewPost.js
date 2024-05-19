ef0c4ce865cf41ff1be554a8d2bba6c9b5967d33
wdalbock <126948288+wdalbock@users.noreply.github.com>
2024-05-19 11:40:34 +0800

    Slight fix to issue where comments were not submitting with 'enter'

--- app/static/javascript/viewPost.js
+++ app/static/javascript/viewPost.js
@@ -1,7 +1,7 @@
 $commentInput = $('#comment-box')
 
 $commentInput.keydown(function (e) { //Ensures characters which are not allowed in the comment box are not allowed to be inserted. 
-    if (!e.key.match(/^[a-zA-Z ,.!?]+$/) || e.which == 13) {
+    if (!e.key.match(/^[a-zA-Z ,.!?]+$/)) {
         e.preventDefault();
     }
 });
